751740d659b18f3e0d752c3abd2a87a374de0a6c
경칠이람 <kkc1590753@gmail.com>
2025-11-13 09:56:22 +0900

    feat: 초안

---
+++ Assets/Scripts/Item/ItemObject.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IInteractable
+{
+    public string GetInteractPrompt();
+    public void Oninteract();
+}
+
+public class ItemObject : MonoBehaviour, IInteractable
+{
+    public ItemData data;
+
+    public string GetInteractPrompt()
+    {
+        string str = $"{data.displayName}\n{data.description}";
+        return str;
+    }
+
+    public void Oninteract()
+    {
+        CharacterManager.Instance.Player.itemData = data;
+        CharacterManager.Instance.Player.addItem?.Invoke();
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/UI/ItemSlot.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemSlot : MonoBehaviour
+{
+    public ItemData item;
+
+    public Button button;
+    public Image icon;
+    public TextMeshProUGUI quantityText;
+    private Outline outline;
+
+    public UIInventory inventory;
+
+    public int index;
+    public bool equipped;
+    public int quantity;
+
+
+    private void Awake()
+    {
+        outline = GetComponent<Outline>();
+    }
+
+    private void OnEnable()
+    {
+        outline.enabled = equipped;
+    }
+
+    public void Set()
+    {
+        if (item == null)
+        {
+            Debug.LogWarning($"[ItemSlot] Tried to Set() but item is null (slot index: {index})");
+            Clear();
+            return;
+        }
+
+        if (icon == null || quantityText == null)
+        {
+            Debug.LogError($"[ItemSlot] UI references missing! icon={icon}, quantityText={quantityText}");
+            return;
+        }
+
+        icon.gameObject.SetActive(true);
+        icon.sprite = item.icon;
+        quantityText.text = quantity > 1 ? quantity.ToString() : string.Empty;
+
+        if (outline != null)
+        {
+            outline.enabled = equipped;
+        }
+    }
+
+    public void Clear()
+    {
+        item = null;
+        icon.gameObject.SetActive(false);
+        quantityText.text = string.Empty;
+    }
+
+    public void OnClickButton()
+    {
+        inventory.SelectItem(index);
+    }
+}
